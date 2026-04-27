5da9fa9f0b3b1d924f1422166f7842f6e1fbc569
Sohrab Tawana <sohrab.tawana@tum.de>
2026-04-27 09:16:33 +0200

    refactor: update paths, increase shard range, and add execution duration logging to activation collection script

--- scripts/submit_collect.sh
+++ scripts/submit_collect.sh
@@ -6,12 +6,13 @@
 #SBATCH -e logs/collect_activations_%j.err
 
 # Define Paths
-CODE_DIR="/dss/dsshome1/08/ga25ley2/code/InterPLM"
+INTERPLM_DIR="/dss/dsshome1/08/ga25ley2/code/InterPLM"
 CROSSCODE_DIR="/dss/dsshome1/08/ga25ley2/code/crosscode"
-DATA_DIR="/dss/dssfs02/lwp-dss-0001/pn67na/pn67na-dss-0000/ga25ley2"
+DATA_DIR="/dss/dssfs02/lwp-dss-0001/pn67na/pn67na-dss-0000/ga25ley2/data"
+CKPT_DIR="/dss/dssfs02/lwp-dss-0001/pn67na/pn67na-dss-0000/ga25ley2/model_checkpoints"
 
 # Mounts: Host:Container
-MOUNTS="${CODE_DIR}:/workspace/InterPLM,${DATA_DIR}:/workspace/data,${CROSSCODE_DIR}:/workspace/crosscode"
+MOUNTS="${INTERPLM_DIR}:/workspace/InterPLM,${DATA_DIR}:/workspace/data,${CKPT_DIR}:/workspace/model_checkpoints,${CROSSCODE_DIR}:/workspace/crosscode"
 
 # Env
 export HF_HOME="/workspace/data/hf_home"
@@ -19,7 +20,8 @@ export PYTHONPATH="/workspace/InterPLM"
 
 mkdir -p logs
 
-echo "Starting Feature Activation Collection on $(hostname) at $(date)"
+echo "Starting InterPLM collect activations on $(hostname) at $(date)"
+START_TIME=$(date +%s)
 
 # Use Python 3.12 to satisfy crosscode requirements
 srun --container-image="nvcr.io/nvidia/pytorch:25.12-py3" \
@@ -31,7 +33,12 @@ srun --container-image="nvcr.io/nvidia/pytorch:25.12-py3" \
      uv pip install -e /workspace/crosscode && \
      uv pip install -e . && \
      uv run scripts/collect_feature_activations.py \
-     --sae_dir /workspace/data/checkpoints/crosscoder_l8192_k32_bs512_full_2026-03-12_06-03-41/crashed_epoch_0_step_2519836 \
-     --embeddings_dir /workspace/data/uniprotkb_modern_score5_5k/analysis_embeddings/prott5/layer_crosscoder \
-     --metadata_dir /workspace/data/uniprotkb_modern_score5_5k/processed_annotations \
-     --shard_range 0 7"
+     --sae_dir /workspace/model_checkpoints/crosscoder_l8192_k32_bs512_full_2026-03-12_06-03-41/crashed_epoch_0_step_2519836 \
+     --embeddings_dir /workspace/data/uniprotkb_modern_score5_35k/analysis_embeddings/prott5/layer_crosscoder \
+     --metadata_dir /workspace/data/uniprotkb_modern_score5_35k/processed_annotations \
+     --shard_range 0 41"
+END_TIME=$(date +%s)
+DURATION=$((END_TIME - START_TIME))
+
+echo "Collect activations job finished at $(date)"
+echo "Total duration: $((DURATION / 3600))h $((DURATION % 3600 / 60))m $((DURATION % 60))s"
